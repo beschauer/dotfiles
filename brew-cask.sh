da0d643731487d513d5fa339fe7f6a3e1c54e130
Bernhard Schauer <bernhard.schauer@openforce.com>
2019-06-27 21:38:12 +0200

    install stuff with brew

---
+++ brew-cask.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+# to maintain cask ....
+#     brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup`
+
+
+# Install native apps
+
+brew install caskroom/cask/brew-cask
+# brew tap caskroom/versions
+
+# daily
+brew cask install spectacle
+brew cask install nightowl
+
+# dev
+brew cask install iterm2
+
+# browsers
+brew cask install google-chrome
+brew cask install firefox
+
+
+# misc
+brew cask install keka
+brew cask install evernote
+
+
+# Not on cask but I want regardless.
+
