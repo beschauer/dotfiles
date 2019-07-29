cca75e08460f85f277dce25a99174c86ac71c8ba
Bernhard Schauer <bernhard.schauer@openforce.com>
2019-07-29 17:33:23 +0200

    add copy script
    use rsync --dry-run till I feel comfortable with this script

---
+++ copy-to-home.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
+
+function doIt() {
+  echo "rsync dry-run for now"
+  rsync \
+    --dry-run \
+    --exclude ".git/" \
+    --exclude ".DS_Store" \
+    --exclude ".osx" \
+    --exclude "copy-to-home.sh" \
+    --exclude "install.sh" \
+    --exclude "README.md" \
+    --exclude "LICENSE-MIT.txt" \
+    -avh --no-perms . ~;
+  source ~/.bash_profile;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+  doIt;
+else
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+  echo "";
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt;
+  fi;
+fi;
+unset doIt;
+
