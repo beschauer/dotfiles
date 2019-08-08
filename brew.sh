a02875fc6cce4c29d18e5055abaeb75920201fd0
Bernhard Schauer <bernhard.schauer@ituri.local>
2019-08-08 07:52:20 +0200

    Add macvim

--- brew.sh
+++ brew.sh
@@ -31,6 +31,7 @@ brew install wget --with-iri
 
 # Install more recent versions of some OS X tools
 brew install vim --with-override-system-vi
+brew install macvim
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
